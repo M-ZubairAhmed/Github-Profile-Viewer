08861174dbce0292208009bf9477065cf6e1cef1
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-06-11 16:37:50 +0530

    event handler to search

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import { FormGroup, FormControl, Glyphicon, InputGroup } from "react-bootstrap";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userSearchQuiery: ""
+    };
+  }
+
+  getInputValue(event) {
+    this.setState({ userSearchQuiery: event.target.value });
+  }
+
+  assignInputValue() {
+    const query = this.state.userSearchQuiery;
+    console.log("query:", query);
+  }
+
   render() {
     return (
       <div className="app">
@@ -11,9 +27,16 @@ class App extends Component {
         </div>
         <FormGroup className="searchSection">
           <InputGroup>
-            <FormControl className="inputField" placeholder="Enter User" />
+            <FormControl
+              className="inputField"
+              placeholder="Enter User"
+              onChange={event => this.getInputValue(event)}
+            />
             <InputGroup.Addon>
-              <Glyphicon glyph="search" />
+              <Glyphicon
+                glyph="search"
+                onClick={() => this.assignInputValue()}
+              />
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
