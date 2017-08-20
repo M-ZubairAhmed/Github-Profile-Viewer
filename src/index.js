05b5f1ea106194b38e26d6976e56960e102bbed8
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-20 22:09:30 +0530

    accordion added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './scenes/App.jsx';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Accordion from './components/accordion/Accordion';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/a" component={Accordion} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
--- src/scenes/profile/Profile.jsx
+++ src/scenes/profile/Profile.jsx
@@ -1,53 +1,83 @@
 import React, { Component } from 'react';
-import { Image } from 'react-bootstrap';
+import { Grid, Col, Row, Image } from 'react-bootstrap';
+import { Card, Icon } from 'semantic-ui-react';
+import Accordion from '../../components/accordion/Accordion';
+
 import './profile.css';
 
 class Profile extends Component {
   render() {
     return (
-      <div className="footSection">
-        <img
-          className="cover"
-          alt="cover for users github"
-          src={this.props.avatar_url}
-        />
-        <Image
-          className="profile-pic"
-          alt="users github avatar"
-          src={this.props.avatar_url}
-          circle
-          responsive
-        />
-        <div className="name">
+      <Grid>
+        <Row>
+          <Col lg={10} lgOffset={1} sm={12} md={10} mdOffset={1}>
+            <Image
+              className="cover"
+              alt="cover for users github"
+              src={this.props.avatar_url}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Image
+              className="profile-pic"
+              alt="users github avatar"
+              src={this.props.avatar_url}
+              circle
+              responsive
+            />
+          </Col>
+        </Row>
+        <Row className="name">
           {this.props.name !== null ? this.props.name : this.props.login}
-        </div>
-        <div className="bio">
+        </Row>
+        <Row className="bio">
           {this.props.bio}
-        </div>
-        <div className="location">
+        </Row>
+        <Row className="location">
           {this.props.location}
-        </div>
-        <ul className="social-main">
-          <li className="followers-main">
-            <div className="followers">Followers</div>
-            <div className="followers-count">
+        </Row>
+        <Row>
+          <Col lg={4}>
+            <div className="topInfo-sub">
               {this.props.followers}
             </div>
-          </li>
-          <li className="following-main">
-            <div className="following">Following</div>
-            <div className="following-count">
+            <div className="topInfo-title">Followers</div>
+          </Col>
+          <Col lg={4}>
+            <div className="topInfo-sub">
               {this.props.following}
             </div>
-          </li>
-          <li className="hireable-main">
-            <div className="hirable">Hirable</div>
-            <div className="hirable-count">
+            <div className="topInfo-title">Following</div>
+          </Col>
+          <Col lg={4}>
+            <div className="topInfo-sub">
               {this.props.hireable ? 'Available' : 'Occupied'}
             </div>
-          </li>
-        </ul>
-      </div>
+            <div className="topInfo-title">Hirable</div>
+          </Col>
+        </Row>
+        <Card>
+          <Image src="/assets/images/avatar/large/matthew.png" />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className="date">Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+        <Accordion className="repositories" />
+      </Grid>
     );
   }
 }
